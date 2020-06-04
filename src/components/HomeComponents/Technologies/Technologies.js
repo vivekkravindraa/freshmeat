7cc4cd289bd92d69c0cbabed0645a183560e4565
Vivek Ravindra <vivekkravindraa@gmail.com>
2020-06-04 15:46:44 +0530

    fix: naming at Technologies

--- src/components/HomeComponents/Technologies/Technologies.js
+++ src/components/HomeComponents/Technologies/Technologies.js
@@ -7,34 +7,34 @@ import './Technologies.css';
 
 export default function Technologies() {
     return (
-        <div className="PopularProducts-technologies">
-            <div className="PopularProducts-technologies__image">
-                <div className="PopularProducts-technologies__quality">
-                    <p className="PopularProducts-technologies__quality-percentage">100%</p>
-                    <p className="PopularProducts-technologies__quality-standard">Proven Quality</p>
+        <div className="Technologies">
+            <div className="Technologies__image">
+                <div className="Technologies__quality">
+                    <p className="Technologies__quality-percentage">100%</p>
+                    <p className="Technologies__quality-standard">Proven Quality</p>
                 </div>
             </div>
-            <div className="PopularProducts-technologies__content">
-                <p className="PopularProducts-technologies__title">Technologies</p>
-                <p className="PopularProducts-technologies__sub-title">Quality Monitoring</p>
-                <div className="PopularProducts-technologies__concepts">
-                    <div className="PopularProducts-technologies__concept">
-                        <img className="PopularProducts-technologies__concept-image" src={temperatureS} alt="no_image" height="60" />
-                        <p className="PopularProducts-technologies__concept-title">Temperature Control</p>
+            <div className="Technologies__content">
+                <p className="Technologies__title">Technologies</p>
+                <p className="Technologies__sub-title">Quality Monitoring</p>
+                <div className="Technologies__concepts">
+                    <div className="Technologies__concept">
+                        <img className="Technologies__concept-image" src={temperatureS} alt="no_image" height="60" />
+                        <p className="Technologies__concept-title">Temperature Control</p>
                     </div>
-                    <div className="PopularProducts-technologies__concept">
-                        <img  className="PopularProducts-technologies__concept-image" src={microscopeS} alt="no_image" height="60" />
-                        <p className="PopularProducts-technologies__concept-title">Anti bacterial treatment</p>
+                    <div className="Technologies__concept">
+                        <img  className="Technologies__concept-image" src={microscopeS} alt="no_image" height="60" />
+                        <p className="Technologies__concept-title">Anti bacterial treatment</p>
                     </div>
-                    <div className="PopularProducts-technologies__concept">
-                        <img  className="PopularProducts-technologies__concept-image" src={certifiedS} alt="no_image" height="60" />
-                        <p className="PopularProducts-technologies__concept-title">Laboratory Testing</p>
+                    <div className="Technologies__concept">
+                        <img  className="Technologies__concept-image" src={certifiedS} alt="no_image" height="60" />
+                        <p className="Technologies__concept-title">Laboratory Testing</p>
                     </div>
                 </div>
-                <p className="PopularProducts-technologies__description">
+                <p className="Technologies__description">
                     Cras non dui id ex mattis vehicula. Nullam posuere ligula non libero mollis, non ornare sapien rutrum. Quisque vitae risus venenatis, dignissim felis id, vulputate nisl. Praesent sed felis aliquam, ullamcorper tellus.
                 </p>
-                <button className="PopularProducts-technologies__button">Read More <span>+</span></button>
+                <button className="Technologies__button">Read More <span>+</span></button>
             </div>
         </div>
     )

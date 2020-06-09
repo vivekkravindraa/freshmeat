8edb37ec156f764c17f3629274275203af475834
Vivek Ravindra <vivekkravindraa@gmail.com>
2020-06-09 16:01:14 +0530

    feat: ProductList - mobile responsiveness

--- src/components/ProductComponents/ProductList/ProductList.js
+++ src/components/ProductComponents/ProductList/ProductList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Location from '../../CommonComponents/Location/Location';
 import Filter from '../../CommonComponents/Filter/Filter';
 import PopularProductItems from '../PopularProducts/PopularProductItems';
@@ -10,10 +11,39 @@ import Footer from '../../HomeComponents/Footer/Footer';
 import './ProductList.css';
 
 export default function ProductList() {
+    const breadcrumbItems = [
+        {
+            id: 1,
+            path: "/",
+            content: "Home"
+        },
+        {
+            id: 2,
+            path: "/products",
+            content: "Products"
+        },
+        {
+            id: 3,
+            path: "/product-list",
+            content: "All Products"
+        }
+    ]
+
     return (
         <div className="ProductList">
-            <Location />
-            <div className="ProductList-banner"></div>
+            <div className="ProductList-location">
+                <Location />
+            </div>
+            <div className="ProductList-banner">
+                <p className="ProductList-title">PRODUCT LIST</p>
+                <div className="ProductList-breadcrumb-items">
+                    {breadcrumbItems.map((bread, index) => {
+                        return (
+                        <li key={index} className="ProductList-breadcrumb-item"><Router><Link to={bread.path}>{bread.content}</Link></Router></li>
+                        )
+                    })}
+                </div>
+            </div>
             <div className="ProductList-container">
                 <div className="ProductList-header">
                     <p className="ProductList-header__title">Our Store's</p>
